d7f4fbbda7da08397e89470f9780ea42baf33a63
anhdosavvycomsoftware <anh.do@savvycomsoftware.com>
2020-10-02 14:12:44 +0700

    Handle user login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -11,20 +11,23 @@ import { ApiTags, ApiOkResponse } from '@nestjs/swagger'
 
 import { LocalAuthGuard } from '@/guards/local-auth.guard'
 import { AuthService } from './auth.service'
+import { LoginResponseDto } from './dtos/login.dto'
 import { RegisterRequestDto, RegisterResponseDto } from './dtos/register.dto'
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @ApiOkResponse({ type: LoginResponseDto })
   @Post('login')
   login(@Request() req: any) {
     return this.authService.login(req.user)
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  
   @ApiOkResponse({ type: RegisterResponseDto })
   @Post('register')
   register(@Body() body: RegisterRequestDto) {
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException, UnauthorizedException, BadRequestException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { JwtService } from '@nestjs/jwt'
 import { Repository } from 'typeorm'
 
+import { LoginResponseDto, LoggedInUserDto } from './dtos/login.dto'
 import { RegisterRequestDto, RegisterResponseDto } from './dtos/register.dto'
 import { User } from '@/database/entities/user.entity'
 import { PasswordUtil } from '@/utils/password.util'
@@ -16,14 +17,35 @@ export class AuthService {
     private userRepo: Repository<User>,
   ) {}
 
-  validateUser(email: string, password: string): Promise<any> {
-    return Promise.resolve({
-      userId: Date.now(),
-      email
-    })
-  }
+  /**
+   * Finds a matched user's email and password
+   * and returns required information to implement Passport's local strategy
+   * @param email a unique user's email
+   * @param password a plain user's password
+   * @return unique user information
+   */
+  async validateUser(email: string, password: string): Promise<LoggedInUserDto> {
+    const user = await this.userRepo.findOne({ email })
 
-  async login(user: any) {
+    if (!user) {
+      throw new NotFoundException()
+    }
+
+    if (!PasswordUtil.compare(password, user.password)) {
+      throw new UnauthorizedException()
+    }
+
+    return {
+      userId: user._id.toString(),
+      email: user.email
+    }
+  }
+  /**
+   * Processes authentication and returns an access token
+   * @param user a user properties to sign and generate a token
+   * @return logged-in information including a token
+   */
+  async login(user: LoggedInUserDto): Promise<LoginResponseDto> {
     const payload = { email: user.email, sub: user.userId }
     return {
       accessToken: this.jwtService.sign(payload),
@@ -36,9 +58,22 @@ export class AuthService {
    * @return the registered user's insensitive information
    */
   async register(user: RegisterRequestDto): Promise<RegisterResponseDto> {
+    if (await this.isUserEmailExist(user.email)) {
+      throw new BadRequestException('Email has already existed!')
+    }
+
     user.password = PasswordUtil.hash(user.password)
     const registeredUser: User = await this.userRepo.save(user)
 
     return new RegisterResponseDto(registeredUser)
+  }
+
+  /**
+   * Whether an email existed in the database
+   * @param email a user's email
+   * @return a flag to notice an email existed
+   */
+  async isUserEmailExist(email: string): Promise<boolean> {
+    return !!await this.userRepo.count({ email })
   }
 }
